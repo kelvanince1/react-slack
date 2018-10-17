2f6d2b2a8515bfb52506a444790cf96823de0f10
Kelvan Ince <kelvan@mirafinancial.com>
2018-10-16 21:53:28 -0400

    Add form for validation

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 
 class Register extends Component {
+  state = {
+
+  }
+
+  handleChange = () => {
+
+  }
+
   render() {
     return (
-      <div>
-        Register
-      </div>
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for Dev Chat
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid name="username"
+                icon="user"
+                placeholder="Username"
+                iconPosition="left"
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid name="email"
+                icon="mail"
+                placeholder="user@mail.com"
+                iconPosition="left"
+                onChange={this.handleChange}
+                type="email"
+              />
+              <Form.Input
+                fluid name="password"
+                icon="lock"
+                placeholder="Password"
+                iconPosition="left"
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Form.Input
+                fluid name="passwordConfirmation"
+                icon="repeat"
+                placeholder="Password Confirmation"
+                iconPosition="left"
+                onChange={this.handleChange}
+                type="password"
+              />
+
+              <Button color="orange" size="large" fluid>Submit</Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already a user? <Link to="/login">Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
     );
   };
 }
