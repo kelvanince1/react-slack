9de66c897b1fdb8cff3c5022d46bb2ed687db6e8
Kelvan Ince <kelvan@mirafinancial.com>
2018-10-18 20:54:00 -0400

    Add actions and hook up redux to app

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import { createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import 'semantic-ui-css/semantic.min.css';
+
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from './firebase';
 
-import 'semantic-ui-css/semantic.min.css';
+const store = createStore(() => {}, composeWithDevTools());
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        // setUser(user);
+        this.props.history.push('/');
+      }
+    })
+  }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </Router>
+    )
+  }
+}
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
-);
+const RootWithAuth = withRouter(Root);
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
