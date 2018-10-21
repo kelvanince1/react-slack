55682d8a689b099026e756bc011f6170bec7d28e
Kelvan Ince <kelvan@mirafinancial.com>
2018-10-21 19:52:15 -0400

    Add skeletol code for components to be used in console

---
+++ src/components/MetaPanel/MetaPanel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MetaPanel extends Component {
+  render() {
+    return (
+      <div>Meta Panel</div>
+    )
+  }
+}
+
+export default MetaPanel;
