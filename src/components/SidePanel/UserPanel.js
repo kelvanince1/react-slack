3310605311c40fcd75977a04d24baf5726c23d31
Kelvan Ince <kelvan@mirafinancial.com>
2018-10-21 20:52:52 -0400

    Display avatar next to username if user has one

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
 
 import firebase from '../../firebase';
 
@@ -40,6 +40,7 @@ class UserPanel extends Component {
   }
 
   render() {
+    const { user } = this.state;
     return (
       <Grid style={{ background: '#4c3c4c' }}>
         <Grid.Column>
@@ -48,16 +49,18 @@ class UserPanel extends Component {
               <Icon name="code" />
               <Header.Content>DevChat</Header.Content>
             </Header>
+            <Header style={{ padding: '0.25em' }} as="h4" inverted>
+              <Dropdown
+                trigger={
+                  <span>
+                    <Image src={user.photoURL} spaced="right" avatar />
+                    {user.displayName}
+                  </span>
+                }
+                options={this.dropdownOptions()}
+              />
+            </Header>
           </Grid.Row>
-
-          <Header style={{ padding: '0.25em' }} as="h4" inverted>
-            <Dropdown
-              trigger={
-                <span>{this.state.user.displayName}</span>
-              }
-              options={this.dropdownOptions()}
-            />
-          </Header>
         </Grid.Column>
       </Grid>
     )
